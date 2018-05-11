be3048e8f7fb0ecaf5ee05962ad8c0c5d0bca3db
Christopher Bishop <domingobishop@gmail.com>
2018-05-11 18:06:50 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,38 @@
+</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+			<?php
+			/* footer sidebar */
+			if ( ! is_404() ) : ?>
+				<div id="footer-widgets" class="four">
+					<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
+						<?php dynamic_sidebar( 'sidebar-4' ); ?>
+					<?php endif; ?>
+
+					<?php if ( is_active_sidebar( 'sidebar-5' ) ) : ?>
+						<?php dynamic_sidebar( 'sidebar-5' ); ?>
+					<?php endif; ?>
+
+					<?php if ( is_active_sidebar( 'sidebar-6' ) ) : ?>
+						<?php dynamic_sidebar( 'sidebar-6' ); ?>
+					<?php endif; ?>
+
+					<?php if ( is_active_sidebar( 'sidebar-7' ) ) : ?>
+						<?php dynamic_sidebar( 'sidebar-7' ); ?>
+					<?php endif; ?>
+				</div><!-- #footer-widgets -->
+		<?php endif; ?>
+		<div class="site-info">
+			<img src="https://www.rapidssl.com/assets/shared/images/rapidssl_ssl_certificate.gif">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+			&copy;2014. All rights reserved. |	
+			<a href = "http://www.reallywildflowers.co.uk/accessibility-statement/" accesskey="0" title="Help using this website [Alt+0]">Accessibility Statement</a> |	
+			<a href = "http://www.reallywildflowers.co.uk/terms-conditions/" accesskey="8" title="Terms &amp; Conditions of use [Alt+8]">Terms &amp; Conditions</a> |	
+			<a href = "http://www.reallywildflowers.co.uk/privacy-policy/">Privacy Policy</a> |
+			<a href = "http://www.reallywildflowers.co.uk/about/contact-us/" accesskey="9" title="Get in touch [Alt+9]">Contact Us</a> |	
+			<a href = "http://www.reallywildflowers.co.uk/credits/">Credits</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+
+<?php wp_footer(); ?></body></html>
---
+++ functions.php
@@ -0,0 +1,177 @@
+<?php
+
+
+
+//Allow html in category description
+
+
+
+$filters = array('pre_term_description', 'pre_link_description', 'pre_link_notes', 'pre_user_description');
+
+
+
+foreach ( $filters as $filter ) {
+
+
+
+    remove_filter($filter, 'wp_filter_kses');
+
+
+
+}
+
+
+
+ 
+
+
+
+foreach ( array( 'term_description' ) as $filter ) {
+
+
+
+    remove_filter( $filter, 'wp_kses_data' );
+
+
+
+}
+
+
+
+add_action('after_setup_theme', 'remove_admin_bar');
+
+
+
+
+
+
+
+function remove_admin_bar() {
+
+
+
+if (!current_user_can('administrator') && !is_admin()) {
+
+
+
+  show_admin_bar(false);
+
+
+
+}
+
+
+
+}
+
+
+
+/**
+
+
+
+ * Attach a class to linked images' parent anchors
+
+
+
+ * e.g. a img => a.img img
+
+
+
+ */
+
+
+
+function give_linked_images_class($html, $id, $caption, $title, $align, $url, $size, $alt = '' ){
+
+
+
+  $classes = 'lightbox'; // separated by spaces, e.g. 'img image-link'
+
+
+
+
+
+
+
+  // check if there are already classes assigned to the anchor
+
+
+
+  if ( preg_match('/<a.*? class=".*?">/', $html) ) {
+
+
+
+    $html = preg_replace('/(<a.*? class=".*?)(".*?>)/', '$1 ' . $classes . '$2', $html);
+
+
+
+  } else {
+
+
+
+    $html = preg_replace('/(<a.*?)>/', '$1 class="' . $classes . '" >', $html);
+
+
+
+  }
+
+
+
+  return $html;
+
+
+
+}
+
+
+
+add_filter('image_send_to_editor','give_linked_images_class',10,8);
+
+
+
+
+// Register footer widgets
+register_sidebar( array(
+	'name' => __( 'Footer Widget One', 'tto' ),
+	'id' => 'sidebar-4',
+	'description' => __( 'Found at the bottom of every page (except 404s, optional homepage and full width) as the footer. Left Side.', 'tto' ),
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => '</aside>',
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+) );
+
+register_sidebar( array(
+	'name' => __( 'Footer Widget Two', 'tto' ),
+	'id' => 'sidebar-5',
+	'description' => __( 'Found at the bottom of every page (except 404s, optional homepage and full width) as the footer. Center.', 'tto' ),
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => "</aside>",
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+) );
+
+register_sidebar( array(
+	'name' => __( 'Footer Widget Three', 'tto' ),
+	'id' => 'sidebar-6',
+	'description' => __( 'Found at the bottom of every page (except 404s, optional homepage and full width) as the footer. Right Side.', 'tto' ),
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => "</aside>",
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+) );
+
+register_sidebar( array(
+	'name' => __( 'Footer Widget Four', 'tto' ),
+	'id' => 'sidebar-7',
+	'description' => __( 'Found at the bottom of every page (except 404s, optional homepage and full width) as the footer. Right Side.', 'tto' ),
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => "</aside>",
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+) );
+
+
+
+?>
